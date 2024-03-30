0f5b1cee6bdd7683176058af6b8cdd523b1be478
kamusarihanshi <ahmedizaka9@gmail.com>
2024-03-30 11:36:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+ const title =document.querySelector("main#main")
+ title.remove();
+ const newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+ newHeader.innerHTML = "Ahmed is the champion";
+ 
